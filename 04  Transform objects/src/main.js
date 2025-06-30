21d188497d218a31addbe086fdd0db59d6eb2a4e
priyambuilds <dpriyam45@gmail.com>
2025-06-30 18:22:13 +0530

    building model 2

--- 04  Transform objects/src/main.js
+++ 04  Transform objects/src/main.js
@@ -9,3 +9,26 @@ scene.background = new THREE.Color('#F0F0F0')
 // 2. Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 5
+
+// 3. Object
+const geometry = new THREE.DodecahedronGeometry()
+const material = new THREE.MeshBasicMaterial({ color: '#468585', emissive: '#468585'})
+const mesh = new THREE.Mesh(geometry, material)
+
+const bocGeometry = new THREE.BoxGeometry(2, 0.1, 2)
+const boxMaterial = new THREE.MeshBasicMaterial({ color: '#B4B4B3'})
+const boxMesh = new THREE.Mesh(bocGeometry, boxMaterial)
+boxMesh.position.y = -1.5
+
+scene.add(mesh, boxMesh)
+
+// 4. Light
+const light = new THREE.SpotLight(0x006769, 100)
+light.position.set(1, 1, 1)
+scene.add(light)
+
+// 5. Renderer
+const renderer = new THREE.WebGLRenderer({canvas})
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.render(scene, camera)
