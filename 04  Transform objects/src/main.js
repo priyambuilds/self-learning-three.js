3e31825c7cac3729ba8703826a0ed15036e72b4c
priyambuilds <dpriyam45@gmail.com>
2025-06-30 17:57:55 +0530

    lession 2

---
+++ 04  Transform objects/src/main.js
@@ -0,0 +1,11 @@
+import * as THREE from 'three';
+
+const canvas = document.getElementById('canvas')
+
+// 1. Scene
+const scene = new THREE.Scene()
+scene.background = new THREE.Color('#F0F0F0')
+
+// 2. Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 5
