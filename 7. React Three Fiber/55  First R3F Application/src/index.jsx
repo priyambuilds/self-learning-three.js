d8887f423b25ea9b0f40b35f5ddd77623cbc59be
priyambuilds <aichive404@gmail.com>
2025-08-22 21:16:36 +0530

    created 1st project with R3F

--- 7. React Three Fiber/55  First R3F Application/src/index.jsx
+++ 7. React Three Fiber/55  First R3F Application/src/index.jsx
@@ -1,10 +1,27 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience'
+import * as THREE from "three"
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <>
-        Soon to be a badass R3F application
-    </>
-)
+<mesh>
+    <Canvas
+        // dpr={[1, 2]}
+        gl={{
+            antialias: true,
+            toneMapping: THREE.ACESFilmicToneMapping
+        }}
+        camera={{
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [3, 2, 6]
+        }}
+    >
+        <Experience />
+    </Canvas>
+</mesh>
+) 
