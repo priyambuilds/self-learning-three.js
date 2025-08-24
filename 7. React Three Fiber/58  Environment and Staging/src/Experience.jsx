94cf7333506bdaae1f40da6e7fb37bcedc50c15b
priyambuilds <aichive404@gmail.com>
2025-08-24 10:00:15 +0530

    Learnt Enviornment and staging in R3F

--- 7. React Three Fiber/58  Environment and Staging/src/Experience.jsx
+++ 7. React Three Fiber/58  Environment and Staging/src/Experience.jsx
@@ -1,14 +1,22 @@
 import { useFrame } from '@react-three/fiber'
-import { Sky, BakeShadows, useHelper, OrbitControls } from '@react-three/drei'
+import {Stage, Lightformer, Environment, Sky, BakeShadows, useHelper, OrbitControls } from '@react-three/drei'
 import { useRef } from 'react'
 import { Perf } from 'r3f-perf'
 import * as THREE from "three"
+import {useControls} from "leva"
 
 export default function Experience()
 {
     const directionalLightRef = useRef()
     useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1)
     const cube = useRef()
+
+    const {envMapHeight, envMapRadius, envMapScale, envMapIntensity} = useControls('evniornmentMap', {
+        envMapIntensity: {value: 3.5, min: 0, max: 12},
+        envMapHeight: { value: 7, min: 0, max: 100 },
+        envMapRadius: { value: 28, min: 10, max: 1000 },
+        envMapScale: { value: 100, min: 10, max: 1000 },
+    })
     
     useFrame((state, delta) =>
     {
@@ -17,15 +25,39 @@ export default function Experience()
 
     return <>
 
-        <BakeShadows />
+        {/* <Environment 
+            preset="sunset"
+            ground={{
+                height: envMapHeight,
+                radius: envMapRadius,
+                scale: envMapScale
 
-        <color args={['ivory']} attach="background"/>
+            }}
+        >
+            <color args={['#000000']} attach="background"/>
+            <Lightformer 
+                position-z={-5} 
+                scale={10}
+                color="red"
+                intensity={10}
+                form="ring"
+            />
+            <mesh position-z={-5} scale={10}>
+                <planeGeometry />
+                <meshBasicMaterial color={[100, 0, 0]}/>
+            </mesh>
+        </Environment> */}
+
+
+        {/* <BakeShadows /> */}
+
+        {/* <color args={['ivory']} attach="background"/> */}
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <directionalLight 
+        {/* <directionalLight 
             ref={directionalLightRef} 
             position={ [ 1, 2, 3 ] } 
             intensity={ 1.5 } 
@@ -40,22 +72,34 @@ export default function Experience()
         />
         <ambientLight intensity={ 0.5 } />
 
-        <Sky />
+        <Sky /> */}
 
-        <mesh castShadow position-x={ - 2 }>
+        {/* <mesh position-y={1} castShadow position-x={ - 2 }>
             <sphereGeometry />
-            <meshStandardMaterial color="orange" />
+            <meshStandardMaterial color="orange" envMapIntensity={envMapIntensity} />
         </mesh>
 
-        <mesh castShadow ref={ cube } position-x={ 2 } scale={ 1.5 }>
+        <mesh position-y={1} castShadow ref={ cube } position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+            <meshStandardMaterial envMapIntensity={envMapIntensity} color="mediumpurple" />
+        </mesh> */}
+        
+        <Stage
+            contactShadow={{opacity: 0.2, blur: 3}}
+            environment="sunset"
+            preset="portrait"
+            intensity={2}
+        >
+            <mesh position-y={1} castShadow position-x={ - 2 }>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" envMapIntensity={envMapIntensity} />
+            </mesh>
 
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+            <mesh position-y={1} castShadow ref={ cube } position-x={ 2 } scale={ 1.5 }>
+                <boxGeometry />
+                <meshStandardMaterial envMapIntensity={envMapIntensity} color="mediumpurple" />
+            </mesh>
+        </Stage>
 
     </>
 }
