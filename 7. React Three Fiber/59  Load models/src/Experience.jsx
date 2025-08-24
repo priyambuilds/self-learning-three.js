e6f57316b5125fb1fb47cc2800a2cf8a95349062
priyambuilds <aichive404@gmail.com>
2025-08-24 20:19:25 +0530

    learnt eveything about loading models in R3F

--- 7. React Three Fiber/59  Load models/src/Experience.jsx
+++ 7. React Three Fiber/59  Load models/src/Experience.jsx
@@ -1,31 +1,37 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { Suspense } from 'react';
+import PlaceHolder from './PlaceHolder';
+import Hamburger from './Hamburger';
+import Fox from './Fox';
 
 export default function Experience()
 {
+
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <directionalLight 
+            castShadow 
+            position={ [ 1, 2, 3 ] } 
+            intensity={ 1.5 } 
+            shadow-normalBias={0.04}
+        />
         <ambientLight intensity={ 0.5 } />
-
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
 
         <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
+
+        <Suspense fallback={<PlaceHolder position-y={0.5} scale={[2,3,2]} />}>
+            <Hamburger scale={0.35}/>
+        </Suspense>
+
+        <Fox />
 
     </>
 }
