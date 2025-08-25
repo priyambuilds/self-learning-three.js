33cde8b77d3a7b076b016b33ff1bf487f6e131a9
priyambuilds <aichive404@gmail.com>
2025-08-26 00:34:33 +0530

    Learnt about mouse events in R3F

---
+++ 7. React Three Fiber/62  Mouse Events/src/Experience.jsx
@@ -0,0 +1,63 @@
+import { useFrame } from '@react-three/fiber'
+import { useBVH, meshBounds, useGLTF, OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
+
+export default function Experience()
+{
+    const hamburger = useGLTF('./hamburger.glb')
+    const cube = useRef()
+    
+    useFrame((state, delta) =>
+    {
+        cube.current.rotation.y += delta * 0.2
+    })
+
+    const eventHandler = () => {
+        cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`)
+    }
+
+    return <>
+
+        <OrbitControls makeDefault />
+
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
+
+        <mesh 
+            onClick={(event) => {event.stopPropagation()}}
+            position-x={ - 2 }
+        >
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <mesh 
+            ref={ cube }
+            raycast={meshBounds}
+            position-x={ 2 } 
+            scale={ 1.5 } 
+            onClick={eventHandler}
+            onPointerEnter={() => {document.body.style.cursor = 'pointer'}}
+            onPointerLeave={() => {document.body.style.cursor = 'default'}}
+        >
+
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+        <primitive 
+            object={hamburger.scene}
+            scale={0.25}
+            position-y={0.5}
+            onClick = {(event) => {
+                event.stopPropagation()
+            }}
+        />
+
+    </>
+}
